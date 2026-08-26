c37bbe75d581d591ab056822c619e9270a548b0e
waanne <work.buiminhquan@gmail.com>
2026-08-26 15:02:34 +0700

    fix: relax pytorch-lightning version constraint to support pip 24.1+ metadata check

--- scripts/convergence/download_wheels.sh
+++ scripts/convergence/download_wheels.sh
@@ -9,7 +9,7 @@ mkdir -p $HOME/pip_wheels
 echo "Downloading pure-python wheels..."
 
 pip download \
-    pytorch-lightning==1.6.5 \
+    "pytorch-lightning>=1.6.0,<1.8.0" \
     online-conformal==1.0.2 \
     efficientnet-pytorch==0.7.0 \
     einops==0.4.1 \
