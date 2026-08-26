7720ef82063d0d19e64cf74484777fbb5fe648b1
waanne <work.buiminhquan@gmail.com>
2026-08-26 15:17:59 +0700

    fix: include torchmetrics 0.9.3 wheel download and installation for pytorch-lightning

--- scripts/convergence/download_wheels.sh
+++ scripts/convergence/download_wheels.sh
@@ -15,6 +15,7 @@ echo "Downloading pre-built offline packages..."
 curl -sSL -L "https://files.pythonhosted.org/packages/00/eb/3b2152f9c3a50d265f3e75529254228ace8a86e9a4397f3004f1e3be7825/pytorch_lightning-1.7.7-py3-none-any.whl" -o $HOME/pip_wheels/pytorch_lightning-1.7.7-py3-none-any.whl
 
 # 2. Download pure-python packages via pip --no-deps
+pip download --no-deps torchmetrics==0.9.3 -d $HOME/pip_wheels
 pip download --no-deps online-conformal==1.0.2 -d $HOME/pip_wheels
 pip download --no-deps efficientnet-pytorch==0.7.1 -d $HOME/pip_wheels
 pip download --no-deps einops==0.4.1 -d $HOME/pip_wheels
