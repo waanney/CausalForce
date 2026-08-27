d446f6df31150f78d1aca267230b2b2af36183b8
waanne <work.buiminhquan@gmail.com>
2026-08-27 19:23:38 +0700

    fix: add dill dependency and resolve sub-dependencies for salesforce-merlion offline wheels

--- scripts/convergence/download_wheels.sh
+++ scripts/convergence/download_wheels.sh
@@ -15,16 +15,17 @@ echo "Downloading pre-built offline packages..."
 # 1. Download pytorch-lightning 1.7.7 wheel via curl (using -L to follow redirects)
 curl -sSL -L "https://files.pythonhosted.org/packages/00/eb/3b2152f9c3a50d265f3e75529254228ace8a86e9a4397f3004f1e3be7825/pytorch_lightning-1.7.7-py3-none-any.whl" -o $HOME/pip_wheels/pytorch_lightning-1.7.7-py3-none-any.whl
 
-# 2. Download pure-python packages via pip --no-deps
-pip download --no-deps timm==0.6.13 -d $HOME/pip_wheels
-pip download --no-deps torchmetrics==0.9.3 -d $HOME/pip_wheels
-pip download --no-deps pyDeprecate==0.3.2 -d $HOME/pip_wheels
-pip download --no-deps fsspec==2022.5.0 -d $HOME/pip_wheels
-pip download --no-deps packaging==21.3 -d $HOME/pip_wheels
-pip download --no-deps online-conformal==1.0.2 -d $HOME/pip_wheels
-pip download --no-deps salesforce-merlion -d $HOME/pip_wheels
-pip download --no-deps efficientnet-pytorch==0.7.1 -d $HOME/pip_wheels
-pip download --no-deps einops==0.4.1 -d $HOME/pip_wheels
+# 2. Download packages and their dependencies
+pip download timm==0.6.13 -d $HOME/pip_wheels
+pip download torchmetrics==0.9.3 -d $HOME/pip_wheels
+pip download pyDeprecate==0.3.2 -d $HOME/pip_wheels
+pip download fsspec==2022.5.0 -d $HOME/pip_wheels
+pip download packaging==21.3 -d $HOME/pip_wheels
+pip download online-conformal==1.0.2 -d $HOME/pip_wheels
+pip download salesforce-merlion -d $HOME/pip_wheels
+pip download dill -d $HOME/pip_wheels
+pip download efficientnet-pytorch==0.7.1 -d $HOME/pip_wheels
+pip download einops==0.4.1 -d $HOME/pip_wheels
 
 # 3. Download torchvision and timm ResNet-50 pretrained weights for offline execution
 echo "Downloading ResNet-50 pretrained weights to torch hub cache..."
