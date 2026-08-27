46c6eed6d5b8192818a1b98b4bccc5e48ebd2c7a
waanne <work.buiminhquan@gmail.com>
2026-08-27 19:35:43 +0700

    fix: pin dill==0.3.7 to support Python 3.8 in PyTorch 22.08 container

--- scripts/convergence/download_wheels.sh
+++ scripts/convergence/download_wheels.sh
@@ -15,7 +15,7 @@ echo "Downloading pre-built offline packages..."
 # 1. Download pytorch-lightning 1.7.7 wheel via curl (using -L to follow redirects)
 curl -sSL -L "https://files.pythonhosted.org/packages/00/eb/3b2152f9c3a50d265f3e75529254228ace8a86e9a4397f3004f1e3be7825/pytorch_lightning-1.7.7-py3-none-any.whl" -o $HOME/pip_wheels/pytorch_lightning-1.7.7-py3-none-any.whl
 
-# 2. Download pure-python wheels without triggering heavy C-extension source builds
+# 2. Download pure-python wheels compatible with Python 3.8
 pip download --no-deps timm==0.6.13 -d $HOME/pip_wheels
 pip download --no-deps torchmetrics==0.9.3 -d $HOME/pip_wheels
 pip download --no-deps pyDeprecate==0.3.2 -d $HOME/pip_wheels
@@ -23,7 +23,7 @@ pip download --no-deps fsspec==2022.5.0 -d $HOME/pip_wheels
 pip download --no-deps packaging==21.3 -d $HOME/pip_wheels
 pip download --no-deps online-conformal==1.0.2 -d $HOME/pip_wheels
 pip download --no-deps salesforce-merlion==2.0.2 -d $HOME/pip_wheels
-pip download --no-deps dill -d $HOME/pip_wheels
+pip download --no-deps dill==0.3.7 -d $HOME/pip_wheels
 pip download --no-deps efficientnet-pytorch==0.7.1 -d $HOME/pip_wheels
 pip download --no-deps einops==0.4.1 -d $HOME/pip_wheels
 
